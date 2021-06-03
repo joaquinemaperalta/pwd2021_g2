04ed4bacc12f280431441bf2c198128a48f055f2
pluisss2020 <71100584+pluisss2020@users.noreply.github.com>
2021-06-03 15:30:53 -0300

    Update config.php

--- libreria/config.php
+++ libreria/config.php
@@ -3,7 +3,7 @@
 define ('DB_HOST','localhost');
 define ('DB_USER','root');
 define ('DB_PASS','');
-define ('DB_NAME','bib-g1');
+define ('DB_NAME','bib-g2');
 
 
 // Nombre de la session (puede dejar este mismo)
